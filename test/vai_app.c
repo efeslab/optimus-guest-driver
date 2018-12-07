9a0a711224e8624bf241013a3cb67d02b30e3940
Alkaid <zgf574564920@gmail.com>
2018-12-06 23:54:26 -0500

    parameterize buffer size for vai_app.c

--- test/vai_app.c
+++ test/vai_app.c
@@ -9,6 +9,7 @@
 #include <assert.h>
 
 #include "vai/vai.h"
+#define BUF_SIZE (4096*10)
 struct status_cl {
     uint64_t completion;
     uint64_t n_clk;
@@ -33,14 +34,14 @@ int main(void)
     fd = open(file_name, O_RDWR);
     ret = ioctl(fd, VAI_SET_RESET);
 
-    ptr = mmap(NULL, 8, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    ptr = mmap(NULL, 0x100, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
     bar = ptr;
 
-    test_map = (volatile char*)mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    test_map = (volatile char*)mmap(NULL, 2*BUF_SIZE+1, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     printf("addr: %llx\n", test_map);
     info.user_addr = (uint64_t)test_map;
-    info.length = 4096;
+    info.length = 2*BUF_SIZE+1;
 
     uint64_t mem_base = (uint64_t)test_map;
     ret = ioctl(fd, VAI_SET_MEM_BASE, mem_base);
@@ -48,8 +49,8 @@ int main(void)
     ret = ioctl(fd, VAI_DMA_MAP_REGION, &info);
 
     volatile char *src = test_map;
-    volatile char *dst = test_map + 1024;
-    volatile struct status_cl *stat = test_map + 2048;
+    volatile char *dst = test_map + BUF_SIZE;
+    volatile struct status_cl *stat = test_map + 2*BUF_SIZE;
 
     uint64_t guidl, guidh;
     guidl = bar[1];
@@ -59,7 +60,7 @@ int main(void)
 
     assert(((uint64_t)test_map)%64 == 0);
 
-    for (i=0; i<1024; i++) {
+    for (i=0; i<BUF_SIZE; i++) {
         src[i] = rand()%256;
         dst[i] = 0;
     }
@@ -75,7 +76,7 @@ int main(void)
     while (stat->completion == 0) {
     }
 
-    for (i=0; i<1024; i++) {
+    for (i=0; i<BUF_SIZE; i++) {
         if (src[i] != dst[i]) {
             printf("fuck it! [%d] is %d, should be %d\n", i, dst[i], src[i]);
             break;
